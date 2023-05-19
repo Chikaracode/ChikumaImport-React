65b21d56d548cfc7008db6b5c8304eadded98d67
Naomi Dezar <chikara.dev97@gmail.com>
2023-05-19 09:13:03 -0500

    se agregaron variables de entorno

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7ag4kW8gx1DryxfREd_r30EBjDr77FyE",
-  authDomain: "chikuma-ecommerce.firebaseapp.com",
-  projectId: "chikuma-ecommerce",
-  storageBucket: "chikuma-ecommerce.appspot.com",
-  messagingSenderId: "585308674112",
-  appId: "1:585308674112:web:5ee08b075b96caea1427a5"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
 };
 
 
